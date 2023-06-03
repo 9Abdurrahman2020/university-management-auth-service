a18a39506b9f773d596f28a3ad20b91e698fbb84
Abdur Rahman <9abdurrahman2020@gmail.com>
2023-06-03 10:39:04 +0600

    tesing

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from 'mongoose';
-import app from './app';
+import mongoose from 'mongoose'
+import app from './app'
 import config from './config/index'
 
-const port = config.port || 5000;
+const port = config.port || 5000
 
-(async function main() {
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log("Database connected successfully");
-        app.listen(port,()=>{
-            console.log("Server is listening on port", port)
-        })
-    }catch(err){
-        console.log("Couldn't connect to database",err)
-    }
-})();
+;(async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected successfully')
+    app.listen(port, () => {
+      console.log('Server is listening on port', port)
+    })
+  } catch (err) {
+    console.log("Couldn't connect to database", err)
+  }
+})()
