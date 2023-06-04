17fb1421c70692ff42d541d73fcec0863690de06
Abdur Rahman <9abdurrahman2020@gmail.com>
2023-06-04 14:54:36 +0600

    customize logger

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,29 +1,33 @@
 import path from 'path';
-import winston from 'winston';
+import { createLogger, format, transports } from 'winston';
+const { combine, timestamp, printf } = format;
 
-const infoLogger = winston.createLogger({
+const myFormat = printf(({ level, message, timestamp }) => {
+  const date = new Date(timestamp);
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const miliSec = date.getMilliseconds();
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds}:${miliSec} ${level}: ${message}`;
+});
+
+const infoLogger = createLogger({
   level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
+  format: combine(timestamp(), myFormat),
   transports: [
-    new winston.transports.Console({ format: winston.format.simple() }),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'info.log'),
       level: 'info',
     }),
   ],
 });
-const errorLogger = winston.createLogger({
+const errorLogger = createLogger({
   level: 'error',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
+  format: combine(timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
       level: 'error',
     }),
